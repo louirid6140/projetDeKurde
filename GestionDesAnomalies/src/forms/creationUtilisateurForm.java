713c36e2ebd81d3d9fbbbba90847348b1e5fe236
Louir <Louir@192.168.60.19>
2014-01-22 18:56:26 +0100

    MVC1

--- GestionDesAnomalies/src/forms/creationUtilisateurForm.java
+++ GestionDesAnomalies/src/forms/creationUtilisateurForm.java
@@ -1,5 +1,67 @@
 package forms;
 
-public class creationUtilisateurForm {
+import javax.servlet.http.HttpServletRequest;
 
+import beans.Utilisateur;
+
+public class creationUtilisateurForm {
+	private static final String CHAMP_NOM   = "nomUtilisateur";
+	private static final String CHAMP_PRENOM   = "prenomUtilisateur";
+	private static final String CHAMP_MAIL   = "emailUtilisateur";
+	private static final String CHAMP_LOGIN   = "loginUtilisateur";
+	private static final String CHAMP_PASSWORD   = "passwordUtilisateur";
+	
+	private String message;
+	private Boolean erreur;
+	
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+	public Boolean getErreur() {
+		return erreur;
+	}
+
+	public Utilisateur CreerUtilisateur(HttpServletRequest request) {
+		String nom = getValeurChamp( request, CHAMP_NOM );
+		String prenom = getValeurChamp( request, CHAMP_PRENOM );
+		String adresse = getValeurChamp( request, CHAMP_MAIL  );
+		String login = getValeurChamp( request, CHAMP_LOGIN);
+		String password = getValeurChamp( request, CHAMP_PASSWORD  );
+		Utilisateur util = new Utilisateur();
+		util.setNom( nom );
+		util.setPrenom( prenom );
+		util.setEmail( adresse );
+		util.setLogin( login );
+		util.setPassword( password);
+		
+		if ( nom.trim().isEmpty() || prenom.trim().isEmpty() ||
+				adresse.trim().isEmpty() ||login.trim().isEmpty() ||password.trim().isEmpty() ) {
+			message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerUtilisateur.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un utilisateur.";
+			erreur = true;
+		} else {
+			message = "Utilisateur créé avec succès !";
+			erreur = false;
+		}
+		
+		return(util);
+	}
+
+	/*
+	 * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
+	 * sinon.
+	 */
+	private static String getValeurChamp( HttpServletRequest
+			request, String nomChamp ) {
+		String valeur = request.getParameter( nomChamp );
+		if ( valeur == null || valeur.trim().length() == 0 ) {
+			return "";
+		} else {
+			return valeur;
+		}
+	}
+	
 }
+
--- GestionDesAnomalies/src/servlets/CreationUtilisateur.java
+++ GestionDesAnomalies/src/servlets/CreationUtilisateur.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import forms.creationUtilisateurForm;
 import beans.Utilisateur;
 
 /**
@@ -16,11 +17,7 @@ import beans.Utilisateur;
 @WebServlet("/CreationUtilisateur")
 public class CreationUtilisateur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static final String CHAMP_NOM   = "nomUtilisateur";
-	public static final String CHAMP_PRENOM   = "prenomUtilisateur";
-	public static final String CHAMP_MAIL   = "emailUtilisateur";
-	public static final String CHAMP_LOGIN   = "loginUtilisateur";
-	public static final String CHAMP_PASSWORD   = "passwordUtilisateur";
+
 	
 	public static final String ATT_UTILISATEUR     = "utilisateur";
 	public static final String ATT_MESSAGE     = "message";
@@ -50,42 +47,15 @@ public class CreationUtilisateur extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nom = request.getParameter( CHAMP_NOM );
-		String prenom = request.getParameter( CHAMP_PRENOM );
-		String adresse = request.getParameter( CHAMP_MAIL );
-		String login = request.getParameter( CHAMP_LOGIN );
-		String password = request.getParameter( CHAMP_PASSWORD );
-		String message;
-		boolean erreur;
-
-		/*
-		 * Initialisation du message à afficher : si un des champs
-		        obligatoires* du formulaire n'est pas renseigné, alors on affiche un message
-		 * d'erreur, sinon on affiche un message de succès
-		 */
-		if ( nom.trim().isEmpty() || prenom.trim().isEmpty() ||
-				adresse.trim().isEmpty() ||login.trim().isEmpty() ||password.trim().isEmpty() ) {
-			message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerUtilisateur.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un utilisateur.";
-			erreur = true;
-		} else {
-			message = "Utilisateur créé avec succès !";
-			erreur = false;
-		}
-
-		/*
-		 * Création du bean Client et initialisation avec les données récupérées
-		 */
-		Utilisateur util = new Utilisateur();
-		util.setNom( nom );
-		util.setPrenom( prenom );
-		util.setEmail( adresse );
-		util.setLogin( login );
-		util.setPassword( password);
-		
+		creationUtilisateurForm utilForm = new creationUtilisateurForm();
+		Utilisateur util=utilForm.CreerUtilisateur(request);
+		String message=utilForm.getMessage();
+		boolean erreur=utilForm.getErreur();
 		/* Ajout du bean et du message à l'objet requête */
 		request.setAttribute( ATT_UTILISATEUR,util );
 		request.setAttribute( ATT_MESSAGE, message );
 		request.setAttribute( ATT_ERREUR, erreur );
+
 		/* Transmission à la page JSP en charge de l'affichage des données */
 		this.getServletContext().getRequestDispatcher(VUE_SUCCES).forward( request, response );
 	}
